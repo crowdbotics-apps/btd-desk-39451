a169634f1f2b6de35c4bf18cad7ea7008637177a
Jonathan Owen <jo@bethedisc.com>
2023-03-21 17:23:23 -0400

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/Login(2027:6611)1295156_2027_6611/index.js
+++ screens/Login(2027:6611)1295156_2027_6611/index.js
@@ -1,35 +1,17 @@
-import React from "react"
-import {
-  View,
-  Image,
-  ImageBackground,
-  TouchableOpacity,
-  Text,
-  Button,
-  Switch,
-  TextInput,
-  StyleSheet,
-  ScrollView
-} from "react-native"
-import Icon from "react-native-vector-icons/FontAwesome"
-import { CheckBox } from "react-native-elements"
-import { connect } from "react-redux"
-import {
-  widthPercentageToDP as wp,
-  heightPercentageToDP as hp
-} from "react-native-responsive-screen"
-import { getNavigationScreen } from "@screens"
+import React from "react";
+import { View, ImageBackground, TouchableOpacity, Text, StyleSheet, ScrollView } from "react-native";
+import { connect } from "react-redux";
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from "react-native-responsive-screen";
+import { getNavigationScreen } from "@screens";
 export class Blank extends React.Component {
   constructor(props) {
-    super(props)
-
-    this.state = {}
+    super(props);
+    this.state = {};
   }
-  render = () => (
-    <ScrollView
-      contentContainerStyle={{ flexGrow: 1 }}
-      style={styles.ScrollView_1}
-    >
+
+  render = () => <ScrollView contentContainerStyle={{
+    flexGrow: 1
+  }} style={styles.ScrollView_1}>
       <View style={styles.View_2} />
       <View style={styles.View_2027_6612}>
         <Text style={styles.Text_2027_6612}>Need an account?</Text>
@@ -40,43 +22,29 @@ export class Blank extends React.Component {
           and Privacy Policy.
         </Text>
       </View>
-      <TouchableOpacity
-        style={styles.TouchableOpacity_2027_6614}
-        onPress={() =>
-          this.props.navigation.navigate(getNavigationScreen("2058_8550"))
-        }
-      >
+      <TouchableOpacity style={styles.TouchableOpacity_2027_6614} onPress={() => this.props.navigation.navigate(getNavigationScreen("2058_8550"))}>
         <Text style={styles.Text_2027_6614}>Sign Up</Text>
       </TouchableOpacity>
-      <TouchableOpacity
-        source={{
-          uri:
-            "https://figma-alpha-api.s3.us-west-2.amazonaws.com/images/a2d852c0-1d7f-449d-b556-f74daec1765a"
-        }}
-        style={styles.TouchableOpacity_2241_12377}
-        onPress={() =>
-          this.props.navigation.navigate(getNavigationScreen("2027_6619"))
-        }
-      />
-      <ImageBackground
-        source={{
-          uri:
-            "https://figma-alpha-api.s3.us-west-2.amazonaws.com/images/10d5def0-d191-4515-b92e-cbd70eb09576"
-        }}
-        style={styles.ImageBackground_2606_10596}
-      />
+      <TouchableOpacity source={{
+      uri: "https://figma-alpha-api.s3.us-west-2.amazonaws.com/images/a2d852c0-1d7f-449d-b556-f74daec1765a"
+    }} style={styles.TouchableOpacity_2241_12377} onPress={() => this.props.navigation.navigate(getNavigationScreen("2027_6619"))} />
+      <ImageBackground source={{
+      uri: "https://figma-alpha-api.s3.us-west-2.amazonaws.com/images/10d5def0-d191-4515-b92e-cbd70eb09576"
+    }} style={styles.ImageBackground_2606_10596} />
       <View style={styles.View_2080_8988}>
         <Text style={styles.Text_2080_8988}>
           © BeTheDisc 2022 | Terms &amp; Conditions | Privacy Policy
         </Text>
       </View>
-    </ScrollView>
-  )
+    </ScrollView>;
 }
-
 const styles = StyleSheet.create({
-  ScrollView_1: { backgroundColor: "rgba(255, 255, 255, 1)" },
-  View_2: { height: hp("113%") },
+  ScrollView_1: {
+    backgroundColor: "rgba(255, 255, 255, 1)"
+  },
+  View_2: {
+    height: hp("113%")
+  },
   View_2027_6612: {
     width: wp("36%"),
     minWidth: wp("36%"),
@@ -180,12 +148,14 @@ const styles = StyleSheet.create({
     letterSpacing: 0,
     textTransform: "none"
   }
-})
+});
 
 const mapStateToProps = state => {
-  return {}
-}
+  return {};
+};
+
 const mapDispatchToProps = () => {
-  return {}
-}
-export default connect(mapStateToProps, mapDispatchToProps)(Blank)
+  return {};
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Blank);
